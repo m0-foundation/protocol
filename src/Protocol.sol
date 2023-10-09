ebb8d2627b194378546f7709b2587679f209a339
toninorair <antonina.norair@gmail.com>
2023-10-09 16:30:47 -0400

    Fixes

--- src/Protocol.sol
+++ src/Protocol.sol
@@ -2,76 +2,61 @@
 
 pragma solidity 0.8.19;
 
+import { Bytes32AddressLib } from "solmate/utils/Bytes32AddressLib.sol";
+
 import { IProtocol } from "./interfaces/IProtocol.sol";
 import { ISPOG } from "./interfaces/ISPOG.sol";
+import { IUpdateCollateralSigVerifier } from "./interfaces/IUpdateCollateralSigVerifier.sol";
 
 contract Protocol is IProtocol {
-    /******************************************************************************************************************\
-    |                                                Protocol Registry Constants                                       |
-    \******************************************************************************************************************/
-    bytes32 public constant MINTERS_LIST_NAME = "minters";
-    bytes32 public constant VALIDATORS_LIST_NAME = "validators";
+    using Bytes32AddressLib for bytes32;
 
+    // Protocol variables
     address public immutable spog;
 
-    // string internal _name;
-    // string internal _version;
-
-    // /// @dev The EIP-712 typehash for the contract's domain
-    // bytes32 internal constant DOMAIN_TYPEHASH =
-    //     keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
-
-    // /// @dev The EIP-712 typehash for updateCollateral action
-    // bytes32 internal constant UPDATE_COLLATERAL_TYPEHASH =
-    //     keccak256("UpdateCollateral(address minter,uint256 amount,uint256 nonce,uint256 expiry)");
-
-    // /// @dev The highest valid value for s in an ECDSA signature pair (0 < s < secp256k1n ÷ 2 + 1)
-    // ///  See https://ethereum.github.io/yellowpaper/paper.pdf #307)
-    // uint internal constant MAX_VALID_ECDSA_S = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0;
-
-    // /// @notice The next expected nonce for a minter, for validating update collateral via signature
-    // mapping(address => uint) public minterNonce;
+    // SPOG lists and variables names
+    bytes32 public constant MINTERS_LIST_NAME = "minters";
+    bytes32 public constant VALIDATORS_LIST_NAME = "validators";
+    bytes32 public constant COLLATERAL_SIG_VERIFIER = "collateral_sig_verifier";
 
     constructor(address spog_) {
         spog = spog_;
-
-        // name = "M Protocol";
-        // version = 1;
     }
 
     /******************************************************************************************************************\
     |                                                Minter Functions                                                  |
     \******************************************************************************************************************/
+    function updateCollateral(
+        address minter,
+        uint256 amount,
+        string memory metadata,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        if (!_isApprovedMinter(minter)) revert InvalidMinter();
+
+        address verifierContract = _getCollateralSigVerifier();
+        address validator = IUpdateCollateralSigVerifier(verifierContract).recoverValidator(
+            minter,
+            amount,
+            metadata,
+            nonce,
+            expiry,
+            v,
+            r,
+            s
+        );
+
+        if (!_isApprovedValidator(validator)) revert InvalidValidator();
+
+        // _updateCollateral(minter, amount);
+    }
+
     //
     //
-    // updateCollateral
-    // function updateCollateral(
-    //     address minter,
-    //     uint256 amount,
-    //     uint256 nonce,
-    //     uint256 expiry,
-    //     uint8 v,
-    //     bytes32 r,
-    //     bytes32 s
-    // ) external {
-    //     if (uint256(s) > MAX_VALID_ECDSA_S) revert InvalidValueS();
-    //     // v ∈ {27, 28} (source: https://ethereum.github.io/yellowpaper/paper.pdf #308)
-    //     if (v != 27 && v != 28) revert InvalidValueV();
-    //     bytes32 domainSeparator = keccak256(
-    //         abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), keccak256(bytes(version)), block.chainid, address(this))
-    //     );
-    //     bytes32 structHash = keccak256(abi.encode(UPDATE_COLLATERAL_TYPEHASH, minter, amount, nonce, expiry));
-    //     bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
-    //     address signatory = ecrecover(digest, v, r, s);
-    //     if (signatory == address(0)) revert BadSignatory();
-    //     if (ISPOG(spog).listContains(MINTERS_LIST_NAME, signatory)) revert BadSignatory();
-    //     if (nonce != minterNonce[signatory]++) revert BadNonce();
-    //     if (block.timestamp >= expiry) revert SignatureExpired();
-    //     _updateCollateral(minter, amount);
-    // }
-
-    // function _updateCollateral(address minter, uint256 amount) internal {}
-
     // proposeMint, mint, cancel, freeze
     // burn
     // proposeRedeem, redeem
@@ -97,4 +82,20 @@ contract Protocol is IProtocol {
     // mintRewardsToZeroHolders
     //
     //
+
+    /******************************************************************************************************************\
+    |                                                SPOG Configs                                                      |
+    \******************************************************************************************************************/
+
+    function _isApprovedMinter(address minter) internal view returns (bool) {
+        return ISPOG(spog).listContains(MINTERS_LIST_NAME, minter);
+    }
+
+    function _isApprovedValidator(address validator) internal view returns (bool) {
+        return ISPOG(spog).listContains(VALIDATORS_LIST_NAME, validator);
+    }
+
+    function _getCollateralSigVerifier() internal view returns (address) {
+        return ISPOG(spog).get(COLLATERAL_SIG_VERIFIER).fromLast20Bytes();
+    }
 }
--- src/interfaces/ISPOG.sol
+++ src/interfaces/ISPOG.sol
@@ -3,5 +3,7 @@
 pragma solidity 0.8.19;
 
 interface ISPOG {
+    function get(bytes32 key) external view returns (bytes32 value);
+
     function listContains(bytes32 list, address account) external view returns (bool contains);
 }
