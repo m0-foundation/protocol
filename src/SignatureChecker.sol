26e8275cc46bebe4d4d4d313668f419110c9e286
toninorair <antonina.norair@gmail.com>
2023-10-20 10:15:31 -0400

    split files

--- src/SignatureChecker.sol
+++ src/SignatureChecker.sol
@@ -2,14 +2,7 @@
 
 pragma solidity 0.8.21;
 
-interface IERC1271 {
-    /**
-     * @dev Should return whether the signature provided is valid for the provided data
-     * @param hash      Hash of the data to be signed
-     * @param signature Signature byte array associated with _data
-     */
-    function isValidSignature(bytes32 hash, bytes memory signature) external view returns (bytes4 magicValue);
-}
+import { IERC1271 } from "./interfaces/IERC1271.sol";
 
 library SignatureChecker {
     enum RecoverError {
