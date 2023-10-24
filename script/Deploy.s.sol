807092a1b939af18959326bd9b86e4d3bc101b65
toninorair <antonina.norair@gmail.com>
2023-10-24 13:05:32 -0400

    Adjust to feedback

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,38 +2,15 @@
 
 pragma solidity 0.8.19;
 
-import { Script, console } from "../lib/forge-std/src/Script.sol";
+import { DeployBase } from "./DeployBase.s.sol";
 
-import { Protocol } from "../src/Protocol.sol";
-import { MToken } from "../src/MToken.sol";
+contract Deploy is DeployBase {
+    // NOTE: Ensure this is the current nonce (transaction count) of the deploying address.
+    uint256 internal constant _DEPLOYER_NONCE = 0;
 
-import { ContractHelper } from "../src/libs/ContractHelper.sol";
+    function run() external {
+        (address deployer_, ) = deriveRememberKey(vm.envString("MNEMONIC"), 0);
 
-contract Deploy is Script {
-    uint256 internal constant _DEPLOYER_STARTING_NONCE = 0;
-
-    address internal _deployer;
-    address internal _protocol;
-    address internal _mToken;
-
-    function setUp() public {
-        string memory mnemonic = vm.envString("MNEMONIC");
-
-        (_deployer, ) = deriveRememberKey(mnemonic, 0);
-
-        console.log("deployer: %s", _deployer);
-    }
-
-    function run() public {
-        vm.startBroadcast(_deployer);
-
-        address expectedProtocol_ = ContractHelper.getContractFrom(_deployer, _DEPLOYER_STARTING_NONCE + 1);
-        _mToken = address(new MToken(expectedProtocol_));
-        _protocol = address(new Protocol(_mToken));
-
-        console.log("Protocol address: ", _protocol);
-        console.log("M Token address: ", _mToken);
-
-        vm.stopBroadcast();
+        deploy(deployer_, _DEPLOYER_NONCE);
     }
 }
