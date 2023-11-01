afdf5f54345498b3843ff2cd21486ae1b7a088ae
toninorair <antonina.norair@gmail.com>
2023-10-31 22:47:03 -0400

    Few changes

--- src/Protocol.sol
+++ src/Protocol.sol
@@ -96,30 +96,30 @@ contract Protocol is IProtocol, StatelessERC712 {
         bytes[] calldata signatures_,
         uint256 requiredQuorum_
     ) internal view {
-        if (requiredQuorum_ != validators_.length) revert InvalidSignaturesLength();
+        if (validators_.length < requiredQuorum_) revert NotEnoughValidSignatures();
 
         uint256 validSignaturesNum_ = 0;
 
-        // TODO consider reverting if any of inputs are duplicate or invalid
         for (uint256 index_ = 0; index_ < signatures_.length; index_++) {
             address validator_ = validators_[index_];
 
-            // check that signature is unique and not accounted for
+            // Check that validator address is unique and not accounted for
             bool duplicate_ = index_ > 0 && validator_ <= validators_[index_ - 1];
             if (duplicate_) continue;
 
-            // check that validator is approved by SPOG
+            // Check that validator is approved by SPOG
             bool authorized_ = _isApprovedValidator(validator_);
             if (!authorized_) continue;
 
-            // check that ECDSA or ERC1271 signatures for given digest are valid
+            // Check that ECDSA or ERC1271 signatures for given digest are valid
             bool valid_ = SignatureChecker.isValidSignature(validator_, digest_, signatures_[index_]);
             if (!valid_) continue;
 
-            validSignaturesNum_++;
+            // Stop processing if quorum was reached
+            if (++validSignaturesNum_ == requiredQuorum_) return;
         }
 
-        if (validSignaturesNum_ < requiredQuorum_) revert NotEnoughValidSignatures();
+        revert NotEnoughValidSignatures();
     }
 
     /// @dev Returns the EIP-712 digest for updateCollateral method
