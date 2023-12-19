fd214d7f48cd1299e4442ee1a3e185277bbc521f
toninorair <antonina.norair@gmail.com>
2023-12-19 11:55:48 -0500

    Add differential test for reference

--- test/ffi/exponent.py
+++ test/ffi/exponent.py
@@ -2,22 +2,15 @@ import sys
 import math
 from eth_abi import encode
 
-# python exp.py 
+# python exponent.py 
 # 2.718...
 
 x = float(sys.argv[1])
 y = math.exp(x / 1e4) * 1e12
 
+# Debug
 # print(f"x = {x}")
 # print(f"y = {int(y)}")
-
-# Debug
-# print(y)
-
-# Check y < 2**64
-# assert y < 2**64, f"x = {x}"
-# y = y * 1e18
-#print(f"y = {y}")
 
 # Encode
 y = "0x" + encode(["uint128"], [int(y)]).hex()
