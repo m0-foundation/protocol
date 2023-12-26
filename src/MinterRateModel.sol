df01090b9f9ad3f84cea2babb9257b3825422444
Pierrick Turelier <pierrick@turelier.com>
2023-12-26 17:19:32 +0100

    feat(MinterRateModel): add Natspec doc

--- src/MinterRateModel.sol
+++ src/MinterRateModel.sol
@@ -5,18 +5,30 @@ pragma solidity 0.8.23;
 import { SPOGRegistrarReader } from "./libs/SPOGRegistrarReader.sol";
 
 import { IMinterRateModel } from "./interfaces/IMinterRateModel.sol";
+import { IRateModel } from "./interfaces/IRateModel.sol";
 
+/**
+ * @title Minter Rate Model contract set in TTG (Two Token Governance) Registrar and accessed by Protocol.
+ * @author M^ZERO LABS_
+ */
 contract MinterRateModel is IMinterRateModel {
+    /// @inheritdoc IMinterRateModel
     address public immutable spogRegistrar;
 
+    /**
+     * @notice Constructs the MinterRateModel contract.
+     * @param spogRegistrar_ The address of the SPOG Registrar contract.
+     */
     constructor(address spogRegistrar_) {
         if ((spogRegistrar = spogRegistrar_) == address(0)) revert ZeroSpogRegistrar();
     }
 
+    /// @inheritdoc IMinterRateModel
     function baseRate() public view returns (uint256 baseRate_) {
         return SPOGRegistrarReader.getBaseMinterRate(spogRegistrar);
     }
 
+    /// @inheritdoc IRateModel
     function rate() external view returns (uint256 rate_) {
         return baseRate();
     }
--- src/interfaces/IMinterRateModel.sol
+++ src/interfaces/IMinterRateModel.sol
@@ -4,10 +4,14 @@ pragma solidity 0.8.23;
 
 import { IRateModel } from "./IRateModel.sol";
 
+/// @title Minter Rate Model Interface.
 interface IMinterRateModel is IRateModel {
+    /// @notice Emitted when SPOG Registrar contract address is zero.
     error ZeroSpogRegistrar();
 
+    /// @notice The SPOG Registrar contract address.
     function spogRegistrar() external view returns (address);
 
+    /// @notice The base rate of the Earner Rate Model.
     function baseRate() external view returns (uint256);
 }
