3444ac4ba2bcd69c767311d8ed69278eb9e0fd21
Pierrick Turelier <pierrick@turelier.com>
2024-02-05 16:47:04 -0600

    Revert "fix(MToken): fix mint silent overflow"
    This reverts commit 312890ce5bf43cc784553a3841f0d1a2028724b9.

--- src/MToken.sol
+++ src/MToken.sol
@@ -225,10 +225,8 @@ contract MToken is IMToken, ContinuousIndexing, ERC20Extended {
             _addNonEarningAmount(recipient_, UIntMath.safe240(amount_));
         }
 
-        // NOTE: Need to cast to uint256 to avoid overflowing uint112 before the comparison.
         if (
-            uint256(principalOfTotalEarningSupply) + _getPrincipalAmountRoundedDown(totalNonEarningSupply) >=
-            type(uint112).max
+            principalOfTotalEarningSupply + _getPrincipalAmountRoundedDown(totalNonEarningSupply) >= type(uint112).max
         ) {
             revert OverflowsPrincipalOfTotalSupply();
         }
