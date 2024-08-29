ed31631c7f2fc0ed50176cd0f46df6c91d9aa9d2
Pierrick Turelier <pierrick@turelier.com>
2024-08-29 10:36:18 -0500

    fix(MToken): rename allowance in burn

--- src/MToken.sol
+++ src/MToken.sol
@@ -79,14 +79,14 @@ contract MToken is IMToken, ContinuousIndexing, ERC20Extended {
 
     /// @inheritdoc IMToken
     function burn(address account_, uint256 amount_) external onlyPortal {
-        uint256 spenderAllowance_ = allowance[account_][msg.sender];
+        uint256 portalAllowance_ = allowance[account_][portal];
 
-        if (spenderAllowance_ != type(uint256).max) {
-            if (spenderAllowance_ < amount_)
-                revert IERC20Extended.InsufficientAllowance(msg.sender, spenderAllowance_, amount_);
+        if (portalAllowance_ != type(uint256).max) {
+            if (portalAllowance_ < amount_)
+                revert IERC20Extended.InsufficientAllowance(portal, portalAllowance_, amount_);
 
             unchecked {
-                _setAllowance(account_, msg.sender, spenderAllowance_ - amount_);
+                _setAllowance(account_, portal, portalAllowance_ - amount_);
             }
         }
 
